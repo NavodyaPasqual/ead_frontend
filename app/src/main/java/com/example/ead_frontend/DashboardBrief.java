f4d0200c189ae5e19aa9898c1ffe575bccf549db
Navodya Pasqual <navodyapasqual@gmail.com>
2022-10-25 19:08:42 +0530

    updated elements

---
+++ app/src/main/java/com/example/ead_frontend/DashboardBrief.java
@@ -0,0 +1,28 @@
+package com.example.ead_frontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class DashboardBrief extends AppCompatActivity {
+
+    ImageButton ibtn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_brief);
+
+        ibtn1 = (ImageButton) findViewById(R.id.imageButton13);
+
+        ibtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ead_frontend/ui/home/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.example.ead_frontend.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.ead_frontend.R;
+import com.example.ead_frontend.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class HomeFragment extends Fragment {
+
+    private com.example.ead_frontend.ui.home.HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    // imageview view variable
+
+    ImageButton ibtn1 ;
+    CardView creditCard;
+    ImageButton ibtn2;
+    FloatingActionButton fab1;
+    ImageButton RKibtn01 ;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //
+
+
+        homeViewModel =
+                new ViewModelProvider(this).get(com.example.ead_frontend.ui.home.HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
