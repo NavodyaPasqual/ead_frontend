b0eb7bcb6bfdfe4aa9bff2a270bf0e6147648477
GayashaBudddhi <it19061580@my.sliit.lk>
2022-10-26 18:18:57 +0530

    uis updated

---
+++ app/src/main/java/com/example/ead_frontend/ui/stationOwner/AvailabilityList.java
@@ -0,0 +1,16 @@
+package com.example.ead_frontend.ui.stationOwner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ead_frontend.R;
+
+public class AvailabilityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.availability_list);
+    }
+}
---
+++ app/src/main/java/com/example/ead_frontend/ui/stationOwner/ShedRegistration.java
@@ -0,0 +1,30 @@
+package com.example.ead_frontend.ui.stationOwner;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ead_frontend.R;
+
+public class ShedRegistration extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shed_registration);
+    }
+}
+
+
