ee02cadd9391b4c6d3477394a22090d6230ee055
Navodya Pasqual <navodyapasqual@gmail.com>
2022-10-27 04:34:38 +0530

    UPDATED

--- app/src/main/java/com/example/ead_frontend/ui/user/UserUpdateDelete.java
+++ app/src/main/java/com/example/ead_frontend/ui/user/UserUpdateDelete.java
@@ -2,7 +2,12 @@ package com.example.ead_frontend.ui.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.ead_frontend.R;
 
@@ -12,5 +17,11 @@ public class UserUpdateDelete extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_update_delete);
+    }
+
+    public void sendToProfile(View view) {
+        Intent intent = new Intent(this,  UserProfile.class);
+        ImageButton button = (ImageButton) findViewById(R.id.back);
+        startActivity(intent);
     }
 }
